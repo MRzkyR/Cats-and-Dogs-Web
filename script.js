a1a5e31b3192b4558f77cd34c6c690e7e55eca1f
MRzkyR <93757902+MRzkyR@users.noreply.github.com>
2023-07-06 20:34:50 +0700

    Add detail card UI

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Responsive Navbar  */
+
 const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
 
@@ -7,8 +9,8 @@ if (bar) {
    });
 }
 
-// API
-// Get DOM elements
+/* API */
+
 const searchButton = document.getElementById("search-button");
 const inputKeyword = document.getElementById("input-keyword");
 const catsRadio = document.getElementById("cats");
@@ -17,40 +19,42 @@ const petCards = document.getElementById("pet-cards");
 
 // Event listener for search button
 searchButton.addEventListener("click", function () {
-   console.log('start');
+   console.log("start");
    const keyword = inputKeyword.value.trim();
+   inputKeyword.value = "";
 
    if (catsRadio.checked) {
-      fetchAndRenderData('cats', keyword);
+      fetchAndRenderData("cats", keyword);
    } else if (dogsRadio.checked) {
-      fetchAndRenderData('dogs', keyword);
+      fetchAndRenderData("dogs", keyword);
    } else {
-      Promise.all([fetchData('cats', keyword), fetchData('dogs', keyword)])
+      Promise.all([fetchData("cats", keyword), fetchData("dogs", keyword)])
          .then(([catsData, dogsData]) => {
             const pets = [...catsData, ...dogsData];
             renderCards(pets);
          })
          .catch((error) => {
-            console.error('Error fetching data:', error);
+            console.error("Error fetching data:", error);
          });
    }
-   console.log('end');
+   console.log("end");
 });
 
 // Fetch data from API
 function fetchData(type, keyword) {
-   const url = `https://api.api-ninjas.com/v1/${type}?name=${encodeURIComponent(keyword)}`;
+   const url = `https://api.api-ninjas.com/v1/${type}?name=${encodeURIComponent(
+      keyword
+   )}`;
    const headers = {
       "X-Api-Key": "NQixyJ5MEN4D4GQu/hPqgg==xvYvSjbn3dVSdPm6",
    };
 
-   return fetch(url, { headers })
-      .then((response) => {
-         if (!response.ok) {
-            throw new Error(response.statusText);
-         }
-         return response.json();
-      });
+   return fetch(url, { headers }).then((response) => {
+      if (!response.ok) {
+         throw new Error(response.statusText);
+      }
+      return response.json();
+   });
 }
 
 // Fetch and render data for a specific type (cats or dogs)
@@ -66,14 +70,11 @@ function fetchAndRenderData(type, keyword) {
 
 // Render pet cards
 function renderCards(pets) {
-   let cards = "";
-   pets.forEach((pet) => {
-      cards += showCards(pet);
-   });
+   const cards = pets.map(showCard).join("");
    petCards.innerHTML = cards;
 }
 
-function showCards(data) {
+function showCard(data) {
    return `
       <div class="pet-card">
          <img src="${data.image_link}" alt="${data.name}">
@@ -81,40 +82,33 @@ function showCards(data) {
       </div>`;
 }
 
+/* CARD DETAIL */
 
-// const searchButton = document.getElementById("search-button");
+const modalOverlay = document.querySelector(".modal-overlay");
 
-// searchButton.addEventListener("click", function () {
-//    const inputKeyword = document.getElementById("input-keyword");
-//    const catsRadio = document.getElementById("cats");
-//    const dogsRadio = document.getElementById("dogs");
+document.addEventListener("click", function (e) {
+   if (e.target.closest(".pet-card")) {
+      modalOverlay.style.display = "block";
+      console.log("OK");
+   }
+});
 
-//    if (catsRadio.checked) {
-//       fetchData('cats', inputKeyword.value);
-//    } else if (dogsRadio.checked) {
-//       fetchData('dogs', inputKeyword.value);
-//    } else {
-//       fetchData('cats', inputKeyword.value);
-//       fetchData('dogs', inputKeyword.value);
-//    }
-// });
+// Open the modal
+function openModal() {
+   modalOverlay.style.display = "block";
+}
 
-// function fetchData(type, keyword) {
-//    fetch(`https://api.api-ninjas.com/v1/${type}?name=${keyword}`, {
-//       headers: {
-//          "X-Api-Key": "NQixyJ5MEN4D4GQu/hPqgg==xvYvSjbn3dVSdPm6",
-//       },
-//    })
-//       .then((response) => response.json())
-//       .then((data) => {
-//          let cards = "";
-//          data.forEach((p) => (cards += showCards(p)));
-//          const petCards = document.getElementById("pet-cards");
-//          petCards.innerHTML = cards;
-//       });
-// }
+// Add event listener to open the modal
+// const petCard = document.querySelector(".pet-card");
+// petCard.addEventListener("click", () => (modalOverlay.style.display = "block"));
 
+// Add event listener to close the modal
+const closeButton = document.querySelector(".close-button");
+closeButton.addEventListener(
+   "click",
+   () => (modalOverlay.style.display = "none")
+);
 
-// Footer
+/* Footer */
 
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
